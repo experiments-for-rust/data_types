179024183d8e4c70156f709c9b0a3a5925c0cd77
skmtseiyaku <skmtseiyaku@outlook.com>
2021-11-21 03:07:23 +0900

    edit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
     //scalar
+    const INT1:i8=-1_28; //-128~127
+    const INT2:u8=255; //0~255
+    const INT3:isize=0x00; //based on system architecture
     
+    
+    println!("{},{},{}", INT1, INT2, INT3);
 }
