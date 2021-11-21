d8d9b92dca4882352beeb3a3ebd4df07faf57972
skmtseiyaku <skmtseiyaku@outlook.com>
2021-11-21 13:18:52 +0900

    edited

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,49 @@
+use std::io;
+use std::io::Write; //flush()
+
 fn main() {
     //scalar
     const INT1:i8=-1_28; //-128~127
     const INT2:u8=255; //0~255
     const INT3:isize=0x00; //based on system architecture
-    
-    
-    println!("{},{},{}", INT1, INT2, INT3);
+    const FLOAT1:f64=2.0;
+    const FLOAT2:f32=3.0;
+    let x=2.0; //f64 as default 
+    let t=true; //printed as char "true"
+    let f:bool = false;
+    let a="a";
+    let b:char = 65 as char; //cast between types
+
+    //compound data types
+    let touple:(i32,i64,i128)=(114514,19198110,893931);
+    let (c,d,e)=touple; //assigned in group
+    let f=touple.0; //the value of the first element in touple is assigned to variable f
+    let array1:[i32;5]=[1,2,3,4,5];
+    let array2=[3;5]; //[3,3,3,3,3]
+
+    println!(
+        "{},{},{},{},{},{},{},{},{},{},{},{},{}", 
+        INT1,INT2,INT3,FLOAT1,FLOAT2,x,t,f,a,b,c,d,e
+    );
+    for i in 0..5{
+        println!("a[{}]={}",i,array1[i]);
+    }
+    for i in array2.iter(){
+        println!("directly print the value of array a[]: {}",i);
+    }
+
+    //invalid visit of array
+    print!("enter an array index for a[]: ");
+    io::stdout().flush().unwrap(); //flush stdout when using print!()
+    let mut index = String::new();
+    io::stdin().read_line(&mut index)
+        .expect("read line failed"); //may cause runtime error when index > 4
+
+    let index:usize=index.trim() //remove leading and trailing whitespaces
+        .parse().expect("index entered was not a number");
+    let element = array1[index];
+    println!(
+        "value of the element at index {}: {}",
+        index,element
+    );
 }
